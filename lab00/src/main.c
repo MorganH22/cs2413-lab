28c9da73e6ddd4f15317e7c403b999e63bc249dd
MorganH22 <mohaschk@ttu.edu>
2026-02-14 18:11:49 -0600

    your message

--- lab00/src/main.c
+++ lab00/src/main.c
@@ -4,9 +4,9 @@ int main(void) {
     printf("CS2413 Lab00 Starter Demo\n");
 
     // TODO: Replace the placeholder information below
-    printf("Name: Shawn\n");
-    printf("R#: 11714613\n");
-    printf("Section: 002\n");
+    printf("Name: Morgan Haschke\n");
+    printf("R#: 11959390\n");
+    printf("Section: 505\n");
     printf("Message: I can use GitHub for CS2413 labs!\n");
 
     return 0;
