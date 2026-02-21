d21eb4c6c4ba1596d86b3e4a4b2e1916d9164385
MorganH22 <mohaschk@ttu.edu>
2026-02-20 19:40:14 -0600

    Finish Assignment 1

--- Assignment1/Question3/student.c
+++ Assignment1/Question3/student.c
@@ -27,7 +27,26 @@
 
 int* plusOne(int* digits, int digitsSize, int* returnSize) {
     // TODO: implement
+for (int i = digitsSize - 1; i >= 0; i--) {
 
+        if (digits[i] < 9) {
+            digits[i]++;
+            *returnSize = digitsSize;
+            return digits;
+        }
+
+        digits[i] = 0;
+    }
+    
+    int* newArray = (int*)malloc((digitsSize + 1) * sizeof(int));
+
+    newArray[0] = 1;
+    for (int i = 1; i <= digitsSize; i++) {
+        newArray[i] = 0;
+    }
+
+    *returnSize = digitsSize + 1;
+    return newArray;
     
 }
 
